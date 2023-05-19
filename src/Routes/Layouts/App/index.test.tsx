985f61ca6a1710b0c5d47a9e137c9109b853401a
simonmysun <simonmysun@gmail.com>
2023-05-19 09:44:25 +0200

    eliminate warnings

--- src/Routes/Layouts/App/index.test.tsx
+++ src/Routes/Layouts/App/index.test.tsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import Layout from '.';
+import { BrowserRouter } from 'react-router-dom';
 
 test('renders Layout', () => {
-  render(<Layout />);
+  render(<BrowserRouter><Layout /></BrowserRouter>);
   const linkElement = screen.getByText(/App layout/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/Routes/Layouts/App/index.tsx
+++ src/Routes/Layouts/App/index.tsx
@@ -9,14 +9,14 @@ function Layout() {
   return (
     <div>
       <header className={"navbar navbar-dark sticky-top bg-tuc flex-md-nowrap p-0 shadow"}>
-        <a className={"navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6"} href="#">Indoor Sound Classification</a>
+        <a className={"navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6"} href="/">Indoor Sound Classification</a>
         <button className={"navbar-toggler position-absolute d-md-none collapsed"} type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
           <span className={"navbar-toggler-icon"}></span>
         </button>
         <input className={"form-control form-control-dark w-100 rounded-0 border-0"} type="text" placeholder="Command" aria-label="Command" />
         <div className={"navbar-nav"}>
           <div className={"nav-item text-nowrap"}>
-            <a className={"nav-link px-3"} href="#">Exit</a>
+            <a className={"nav-link px-3"} href="/logout">Exit</a>
           </div>
         </div>
       </header>
@@ -49,7 +49,7 @@ function Layout() {
           <footer className={"text-muted py-5 col-md-9 ms-sm-auto col-lg-10 px-md-4"}>
             <div className={"container"}>
               <p className={"float-end mb-1"}>
-                <a href="#">Back to home</a>
+                <a href="/">Back to home</a>
               </p>
               <p>     </p>
               <p className={"mb-1"}>Lorem ipsum dolor sit amet, consectetur adipiscing &copy; elit, quis nostrud exercitation.</p>
