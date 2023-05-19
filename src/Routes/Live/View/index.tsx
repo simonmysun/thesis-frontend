dcd3c5f6504a1b189a36c91fc50753d16f87ea29
simonmysun <simonmysun@gmail.com>
2023-05-19 09:43:44 +0200

    better mqtt connection

--- src/Routes/Live/View/index.tsx
+++ src/Routes/Live/View/index.tsx
@@ -1,33 +1,21 @@
 import { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import * as mqtt from 'mqtt/dist/mqtt';
 
 import './style.css';
-import { Heatmap, OrderedList } from './../../../Components';
+import { Heatmap, Ridgeline, OrderedList } from './../../../Components';
 
-const connectionOptions = {
-  scheme: 'ws',
-  host: 'mqtt-admin-mys-karlsruhe-0.makelove.expert',
-  port: 80,
-  path: '/mqtt',
-  username: 'test',
-  password: 'TuC',
-  clientId: () => `frontend_${crypto.randomUUID()}`,
-  url: ''
-};
-
-const timeWindow = 60;
-
-connectionOptions.url = `${connectionOptions.scheme}://${connectionOptions.host}:${connectionOptions.port}${connectionOptions.path}`;
+import { connectionOptions } from './../../../API';
 
 function LiveView(props: any) {
   const { deviceID } = useParams();
-  const [currentData, updateData] = useState<VisDatum[]>([]);
-  const [newData, updateNewData] = useState<VisDatum[]>([]);
-  const [client, setClient] = useState<mqtt.MqttClient | null>(null);
-  const [connectStatus, setConnectStatus] = useState('Disconnected');
+  const [ currentData, updateData ] = useState<VisDatum[]>([]);
+  const [ newData, updateNewData ] = useState<VisDatum[]>([]);
+  const [ sampleRate, setSampleRate ] = useState(0);
+  const [ client, setClient ] = useState<mqtt.MqttClient | null>(null);
+  const [ connectStatus, setConnectStatus ] = useState('Disconnected');
   useEffect(() => {
-    console.log('effect connection');
+    // console.log('effect connection');
     setConnectStatus('Connecting');
     const mqttOption = {
       clean: true,
@@ -36,35 +24,34 @@ function LiveView(props: any) {
       username: connectionOptions.username,
       password: connectionOptions.password
     };
-    console.log(`[mqtt] connecting with clientId=${mqttOption.clientId}`);
+    // console.log(`[mqtt] connecting with clientId=${mqttOption.clientId}`);
     setClient(mqtt.connect(connectionOptions.url, mqttOption));
     return () => {
-      console.log('effect connection cleanup');
-      console.log(client);
+      // console.log('effect connection cleanup');
+      // console.log(client);
       if (client === null) {
-        console.log('client not found');
+        // console.log('client not found');
       } else {
-        console.log('[mqtt] disconnect');
+        // console.log('[mqtt] disconnect');
         client.end();
       }
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
   useEffect(() => {
-    console.log('effect listeners');
+    // console.log('effect listeners');
     if (client === null) {
-      console.log('client not found');
+      // console.log('client not found');
     } else {
       client.on('connect', () => {
-        console.log(`[mqtt] connected`);
+        // console.log(`[mqtt] connected`);
         setConnectStatus('Connected');
       });
       client.on('error', (err: Error) => {
-        console.error('Connection error: ', err);
+        // console.error('Connection error: ', err);
         client.end();
       });
       client.on('reconnect', () => {
-        console.log(`[mqtt] reconnecting`);
+        // console.log(`[mqtt] reconnecting`);
         setConnectStatus('Reconnecting');
       });
       client.on('message', (topic: string, payload: object) => {
@@ -73,58 +60,58 @@ function LiveView(props: any) {
         }
         const message = JSON.parse(payload.toString());
         const timestamp: Date = message.timestamp;
+        setSampleRate(message.sampleRate);
+        const sampleRate = message.sampleRate;
         const updates = Object.keys(message.prediction).map((tag: string) => ({ tag: tag, timestamp: timestamp, value: message.prediction[tag] } as VisDatum));
         updateNewData(updates);
         updateData((prevData: VisDatum[]): VisDatum[] => {
-          return [...prevData.filter(d => d.timestamp > new Date((new Date(Date.now()).getTime() - timeWindow * 1000))), ...updates];
+          return [...prevData.filter(d => d.timestamp > new Date((new Date(Date.now()).getTime() - sampleRate * 60))), ...updates];
         });
       });
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [client]);
+  }, [client]); // eslint-disable-line react-hooks/exhaustive-deps
   useEffect(() => {
-    console.log('effect subscription');
+    // console.log('effect subscription');
     const topic = `tele/indoor_sound_classification/${deviceID}/state`;
     if (client === null) {
-      console.log('client not found');
+      // console.log('client not found');
     } else {
       client.subscribe(topic, { qos: 0 }, (error) => {
         if (error) {
-          console.log('Subscribe to topics error', error);
+          // console.log('Subscribe to topics error', error);
           return;
         }
-        console.log(`[mqtt] subscribed topic=${topic}`);
+        // console.log(`[mqtt] subscribed topic=${topic}`);
       });
     }
     return () => {
-      console.log('effect subscription cleanup');
+      // console.log('effect subscription cleanup');
       if (client === null) {
-        console.log('client not found');
+        // console.log('client not found');
       } else {
         client.unsubscribe(topic, (error: Error) => {
           if (error) {
-            console.log('Unsubscribe error', error);
+            // console.log('Unsubscribe error', error);
             return;
           }
-          console.log(`[mqtt] unsubsribed topic=${topic}`);
+          // console.log(`[mqtt] unsubsribed topic=${topic}`);
         });
       }
       updateData([]);
       updateNewData([]);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [deviceID, connectStatus]);
+  }, [deviceID, connectStatus]); // eslint-disable-line react-hooks/exhaustive-deps
   return (
     <div className="row">
       <span className="hidden">App LiveView</span>
+      <h2>{ deviceID }</h2>
+      <hr/>
       <div className="col col-md-7 col-sm-12 heatmap-container">
-        <Heatmap key={deviceID} currentData={currentData} connectStatus={connectStatus} timeWindow={timeWindow} />
+        <Heatmap key={ deviceID } currentData={ currentData } connectStatus={ connectStatus } sampleRate={ sampleRate } />
       </div>
       <div className="col col-md-5 col-sm-12">
-        <OrderedList list={newData} />
+        <OrderedList list={ newData } />
       </div>
-      <Link to="/live/fake_datasource">fake_datasource</Link>,
-      <Link to="/live/fake_datasource2">fake_datasource2</Link>
     </div>
   );
 }
