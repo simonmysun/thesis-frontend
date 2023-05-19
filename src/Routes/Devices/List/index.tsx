c22d03696dfe4da189bfa46803176a9b3d1860ac
simonmysun <simonmysun@gmail.com>
2023-05-19 09:43:20 +0200

    device list view

--- src/Routes/Devices/List/index.tsx
+++ src/Routes/Devices/List/index.tsx
@@ -1,9 +1,43 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { backendApi, connectionOptions } from './../../../API';
+
 import './style.css';
 
 function DeviceList() {
+  const [deviceList, setDeviceList] = useState<{ name: string, comment: string }[]>([]);
+  useEffect(() => {
+    backendApi.devices.list().then(res => {
+      setDeviceList(res);
+    });
+  }, []);
   return (
     <div>
       <span className="hidden">App Device List</span>
+      <h2>Device Management</h2>
+      <hr />
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Device Name</th>
+            <th scope="col">Comment</th>
+            <th scope="col">Operations</th>
+          </tr>
+        </thead>
+        <tbody>
+          { deviceList.map(device => (
+            <tr key={ device.name }>
+              <td>{ device.name }</td>
+              <td>{ device.comment }</td>
+              <td>
+                <button type="button" className="btn btn-primary"><i className="bi bi-pencil"></i></button>&nbsp;
+                <button type="button" className="btn btn-danger"><i className="bi bi-x-lg"></i></button>
+              </td>
+            </tr>
+          )) }
+        </tbody>
+      </table>
+      <button type="button" className="btn btn-success"><i className="bi bi-plus"></i> Add new device</button>
     </div>
   );
 }
