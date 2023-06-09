4e1c23e8fe92796c757fd21b434ef40efacec607
simonmysun <simonmysun@gmail.com>
2023-06-09 07:33:04 +0200

    fix test with fake mqtt server

--- src/Routes/Live/List/index.test.tsx
+++ src/Routes/Live/List/index.test.tsx
@@ -3,6 +3,7 @@ import LiveList from '.';
 import { BrowserRouter } from 'react-router-dom';
 
 test('renders Live List', () => {
+  global.localStorage.setItem('settings', '{"mqttUrl":"wss://fake/mqtt","mqttUsername":"fake","mqttPassword":"fake"}');
   render(<BrowserRouter><LiveList /></BrowserRouter>);
   const linkElement = screen.getByText(/App Live List/i);
   expect(linkElement).toBeInTheDocument();
--- src/Routes/Live/View/index.test.tsx
+++ src/Routes/Live/View/index.test.tsx
@@ -2,8 +2,8 @@ import { render, screen } from '@testing-library/react';
 import LiveView from '.';
 import { BrowserRouter } from 'react-router-dom';
 
-
 test('renders Live View', () => {
+  global.localStorage.setItem('settings', '{"mqttUrl":"wss://fake/mqtt","mqttUsername":"fake","mqttPassword":"fake"}');
   render(<BrowserRouter><LiveView /></BrowserRouter>);
   const linkElement = screen.getByText(/App LiveView/i);
   expect(linkElement).toBeInTheDocument();
