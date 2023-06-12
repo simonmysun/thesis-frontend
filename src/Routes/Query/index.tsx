170ee04c90bea377f7cac1d421ee8ecff572f5b4
simonmysun <simonmysun@gmail.com>
2023-06-12 05:40:22 +0200

    add date range picker

--- src/Routes/Query/index.tsx
+++ src/Routes/Query/index.tsx
@@ -1,17 +1,16 @@
 import Multiselect from 'multiselect-react-dropdown';
-import { DateRangePicker } from 'react-date-range';
+import DateTimeRangeContainer from 'react-advanced-datetimerange-picker';
+import moment, { Moment } from 'moment';
 import { useState, useEffect } from 'react';
 import {
   CalHeatmapHourXDay,
-  CalHeatmapWeekXMonth,
+  CalHeatmapDayXDay,
   HourlyHeatmap,
   StatusTimeline,
   Prediction,
 } from './../../Components';
 import { backendApi } from './../../API';
 
-import 'react-date-range/dist/styles.css'; // main css file
-import 'react-date-range/dist/theme/default.css'; // theme css file
 import './style.css';
 
 const tagList = ['Alarmsignal', 'AstmaHusten', 'Blending', 'Bohren', 'CovidHusten', 'Doseöffnen', 'Electronic_Zahnbürste', 'Etwas-am-Boden-ziehen', 'Fenster', 'Feuerzeug', 'Flöte', 'Fußstapfen-gehen', 'GesunderHusten', 'Gitarre', 'Glas', 'Haartrockner', 'Hahn', 'Handsäge', 'Huhn', 'Hund', 'Katze', 'Klarinette', 'Klassenname', 'klatschen', 'Klingelton', 'Küssen', 'Lachen', 'Mausklick', 'Metall-auf-Metall', 'Möbelrücken', 'Niesen', 'Pfeifen', 'Presslufthammer', 'Ruhe', 'Schlag', 'Schlagzeug', 'Schnarchen', 'Sirene', 'Sitar', 'SprechendeFrau', 'SprechenderMann', 'Staubsauger', 'Tastatur-tippen', 'Toilettenspülung', 'Trampler', 'Trinken', 'Türklingel', 'Türklopfen', 'Uhr-ticken', 'Vandalismus', 'Waschmaschine', 'Wasser', 'Weinen', 'Wimmern', 'Wind', 'Zahnbürste', 'Zerbrechen', 'ZwitscherndeVögel'];
@@ -29,8 +28,6 @@ function Query() {
   const [SelectedTags, setSelectedTags] = useState<{ name: string, id: number }[]>([]);
   const prefix = `https://thesis-backend-grafana-mys-karlsruhe-0.makelove.expert/d-solo/s4WuoHlVz/indoor-sound-classification`;
   const orgId = 1;
-  const rangeFrom = 1685692275647;
-  const rangeTo = 1686297075647
   const refresh = '30s';
   const updateSelectedDevices = (selectedList: any, selectedItem: any) => {
     setSelectedDevices(selectedList);
@@ -38,9 +35,33 @@ function Query() {
   const updateSelectedTags = (selectedList: any, selectedItem: any) => {
     setSelectedTags(selectedList);
   }
+  let now = new Date();
+  let dayStart = moment(new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0));
+  const [rangeFrom, setRangeFrom] = useState<Moment>(moment(now).subtract(15, 'minutes'));
+  const [rangeTo, setRangeTo] = useState<Moment>(moment(now));
+  let ranges = {
+    'Last 15 Minutes': [moment(now).subtract(15, 'minutes'), moment(now)],
+    'Today Only': [moment(dayStart).subtract(1, 'days'), moment(dayStart).subtract(1, 'seconds')],
+    'Yesterday Only': [moment(dayStart).subtract(2, 'days'), moment(dayStart).subtract(1, 'days')],
+    'Last 24 Hours': [moment(now).subtract(1, 'days'), moment(now)],
+    'Last 3 Days': [moment(dayStart).subtract(3, 'days'), moment(dayStart).subtract(1, 'seconds')],
+    'Last Week': [moment(dayStart).subtract(7, 'days'), moment(dayStart).subtract(1, 'seconds')],
+    'Last 14 Days': [moment(dayStart).subtract(14, 'days'), moment(dayStart).subtract(1, 'seconds')],
+    'Last Month': [moment(dayStart).subtract(1, 'month'), moment(dayStart).subtract(1, 'seconds')],
+    'Last Year': [moment(dayStart).subtract(1, 'year'), moment(dayStart).subtract(1, 'seconds')],
+  };
+  let local = {
+    'format': 'DD-MM-YYYY HH:mm',
+    'sundayFirst': false
+  };
+  let maxDate = moment(now);
+  const setRange = (startDate: Moment, endDate: Moment) => {
+    setRangeFrom(startDate);
+    setRangeTo(endDate);
+  };
   return (
     <div>
-      <span className="hidden">App Query</span>
+      <span className='hidden'>App Query</span>
       <div className='scroll-container'>
         <div className='stick-top row'>
           <div className='col-md-4 col-sm-12'>
@@ -73,6 +94,18 @@ function Query() {
               showArrow={true}
             />
           </div>
+          <div className='col-md-4 col-sm-12'>
+            <DateTimeRangeContainer
+              ranges={ranges}
+              start={rangeFrom}
+              end={rangeTo}
+              local={local}
+              maxDate={maxDate}
+              applyCallback={setRange}
+            >
+              <input type='text' className='form-control' id='time-range' placeholder='' value={`From ${rangeFrom.format()} to ${rangeTo.format()}`} readOnly={true}/>
+            </DateTimeRangeContainer>
+          </div>
         </div>
         <div className='non-stick-top'>
           <div className='row'>
@@ -80,51 +113,51 @@ function Query() {
               <Prediction
                 prefix={prefix}
                 orgId={orgId}
-                rangeFrom={rangeFrom}
-                rangeTo={rangeTo}
+                rangeFrom={rangeFrom.valueOf()}
+                rangeTo={rangeTo.valueOf()}
                 refresh={refresh}
-                deviceId={SelectedDevices.map(option => option.name)}
-                tags={SelectedTags.map(option => option.name)} />
+                deviceId={SelectedDevices.length > 0 ? SelectedDevices.map(option => option.name) : ['All']}
+                tags={SelectedTags.length > 0 ? SelectedTags.map(option => option.name) : ['All']} />
             </div>
             <div className='col-sm-12'>
               <CalHeatmapHourXDay
                 prefix={prefix}
                 orgId={orgId}
-                rangeFrom={rangeFrom}
-                rangeTo={rangeTo}
+                rangeFrom={rangeFrom.valueOf()}
+                rangeTo={rangeTo.valueOf()}
                 refresh={refresh}
-                deviceId={SelectedDevices.map(option => option.name)}
-                tags={SelectedTags.map(option => option.name)} />
+                deviceId={SelectedDevices.length > 0 ? SelectedDevices.map(option => option.name) : ['All']}
+                tags={SelectedTags.length > 0 ? SelectedTags.map(option => option.name) : ['All']} />
             </div>
             <div className='col-sm-12'>
-              <CalHeatmapWeekXMonth
+              <CalHeatmapDayXDay
                 prefix={prefix}
                 orgId={orgId}
-                rangeFrom={rangeFrom}
-                rangeTo={rangeTo}
+                rangeFrom={rangeFrom.valueOf()}
+                rangeTo={rangeTo.valueOf()}
                 refresh={refresh}
-                deviceId={SelectedDevices.map(option => option.name)}
-                tags={SelectedTags.map(option => option.name)} />
+                deviceId={SelectedDevices.length > 0 ? SelectedDevices.map(option => option.name) : ['All']}
+                tags={SelectedTags.length > 0 ? SelectedTags.map(option => option.name) : ['All']} />
             </div>
             <div className='col-sm-12 col-md-8'>
               <StatusTimeline
                 prefix={prefix}
                 orgId={orgId}
-                rangeFrom={rangeFrom}
-                rangeTo={rangeTo}
+                rangeFrom={rangeFrom.valueOf()}
+                rangeTo={rangeTo.valueOf()}
                 refresh={refresh}
-                deviceId={SelectedDevices.map(option => option.name)}
-                tags={SelectedTags.map(option => option.name)} />
+                deviceId={SelectedDevices.length > 0 ? SelectedDevices.map(option => option.name) : ['All']}
+                tags={SelectedTags.length > 0 ? SelectedTags.map(option => option.name) : ['All']} />
             </div>
             <div className='col-sm-12 col-md-4'>
               <HourlyHeatmap
                 prefix={prefix}
                 orgId={orgId}
-                rangeFrom={rangeFrom}
-                rangeTo={rangeTo}
+                rangeFrom={rangeFrom.valueOf()}
+                rangeTo={rangeTo.valueOf()}
                 refresh={refresh}
-                deviceId={SelectedDevices.map(option => option.name)}
-                tags={SelectedTags.map(option => option.name)} />
+                deviceId={SelectedDevices.length > 0 ? SelectedDevices.map(option => option.name) : ['All']}
+                tags={SelectedTags.length > 0 ? SelectedTags.map(option => option.name) : ['All']} />
             </div>
           </div>
         </div>
